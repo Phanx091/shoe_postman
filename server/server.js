ac9621be5ca759c2157a6bedd8f45b6c98559a96
Jason Phan <Phanx091@umn.edu>
2018-05-03 10:08:22 -0500

    post is online

--- server/server.js
+++ server/server.js
@@ -1,19 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
-const shoesRouter = require( './routes/shoes.routes');
+const shoesRouter = require("./routes/shoes.routes");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
-app.use(express.static('server/public'));
-
-app.use( '/shoes', shoesRouter);
+app.use("/shoes", shoesRouter);
 
 app.listen(PORT, () => {
-    console.log('listening on port', PORT);
-  });
-  
-  
+  console.log("listening on port", PORT);
+});
